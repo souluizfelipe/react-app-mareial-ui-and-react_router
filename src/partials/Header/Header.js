7d7c145dc071c6f78e498d3c4b04a2a68e4f2c8a
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-22 11:20:49 -0300

    add styles to Header.js and create Heade.styles.js

--- src/partials/Header/Header.js
+++ src/partials/Header/Header.js
@@ -8,15 +8,19 @@ import {
 
 import MenuIcon from '@material-ui/icons/Menu'
 
+import useStyles from './Header.styles'
+
 const Header = () => {
+  const classes = useStyles()
+
   return(
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start"  color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" >
-          News
+        <Typography variant="h6" className={classes.title} >
+          My React App
         </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
