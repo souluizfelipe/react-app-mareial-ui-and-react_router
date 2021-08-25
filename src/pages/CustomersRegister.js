a3b390e0b3420a93140b9ddb2cf6cb372296958b
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-25 14:26:59 -0300

    create customerregister verification form and method post to api

--- src/pages/CustomersRegister.js
+++ src/pages/CustomersRegister.js
@@ -1,8 +1,13 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router'
+
 import {
   TextField, 
   Button, 
   makeStyles 
 } from "@material-ui/core"
+
 
 const useStyles = makeStyles((theme) => ({
   inputWrapper: {
@@ -16,31 +21,102 @@ const useStyles = makeStyles((theme) => ({
 
 const CustomerRegister = () =>  {
   const classes = useStyles()
-  
+  const history = useHistory()
+
+  const [ form, setForm ] = useState({
+    name: {
+      value: '',
+      error: false,
+    },
+    job: {
+      value: '',
+      error: false,
+    }
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    
+    setForm({
+      ...form,
+      [name]: {
+        ...form[name],
+       value,
+      }  
+    })
+  } 
+
+  const hadleRegisterButton = () => {
+    let hasError = false
+    let newFormState = {
+      ...form,
+    }
+
+
+    if(!form.name.value) {
+      hasError = true
+      newFormState.name = {
+        value: form.name.value, 
+        error: true,
+        helperText: 'Invalid entry',
+      }
+    }
+    
+    if(!form.job.value) {
+      hasError = true
+      newFormState.job = {
+        value: form.job.value,
+        error: true,
+        helperText: 'Invalid entry',
+      }
+
+    }
+    
+    if(hasError){
+      return setForm(newFormState)
+    }
+
+    axios.post('https://reqres.in/api/users', {
+      name: form.name.value, 
+      job:  form.job.value,
+    }).then(res => console.log('ok' ,res))
+    
+    history.push('/customers')
+
+  }
+
+
   return (
     <>
       <div className={classes.inputWrapper}>
         <TextField 
           name="name" 
-          id="outlined-basic" 
+          id="name" 
           label="Name" 
           variant="outlined"
           className={classes.input}
+          onChange={handleInputChange}
+          error={form.name.error}
+          helperText={form.name.error ? form.name.helperText : ''}
           />
       </div>
       <div className={classes.inputWrapper}>
         <TextField 
           name="job" 
-          id="outlined-basic" 
+          id="job" 
           label="Job" 
           variant="outlined"
           className={classes.input}
+          onChange={handleInputChange}
+          error={form.job.error}
+          helperText = {form.job.error ? form.job.helperText : ''}
           />
       </div>
       <div className={classes.inputWrapper}>
         <Button 
           variant="contained" 
           color="primary" 
+          onClick={hadleRegisterButton}
         >
           Register
         </Button>
